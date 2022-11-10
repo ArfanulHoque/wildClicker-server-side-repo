4bc1e297b9befbc0fc206c102d1c66529933938c
Arfanul Hoque Erfan <mderfan3928@gmail.com>
2022-11-10 11:09:05 +0600

    get data from database

--- index.js
+++ index.js
@@ -1,6 +1,49 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xasmzpl.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+// client.connect((err) => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+async function run() {
+  try {
+    const serviceCollection = client.db("wildClicker").collection("services");
+    const reviewCollection = client.db("wildClicker").collection("reviews");
+
+    app.get("/findServices", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const result = await cursor.limit(3).toArray();
+      res.send(result);
+    });
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
   res.send("Services API Running");
